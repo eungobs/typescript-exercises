196b9b145c7ce91fe5c9d75d15f2881969de420c
eungobs <eungobs@gmail.com>
2024-06-26 11:50:18 +0200

    type-aliases

---
+++ type-aliases/exercise2.ts
@@ -0,0 +1,8 @@
+// Type Aliases
+
+// Create a type alias for a uniotype that can be either a string or a boolean.
+type StringOrBoolean = string | boolean;
+
+// Example usage:
+let example1: StringOrBoolean = "This is a string";
+let example2: StringOrBoolean = true;
